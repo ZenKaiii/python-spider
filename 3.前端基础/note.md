c2c46d13b71a1f29089ab815f72b514108f7d317
zenkaiii <wintersoul@foxmail.com>
2019-10-14 21:41:29 +0800

    http

---
+++ 3.前端基础/note.md
@@ -0,0 +1,5 @@
+## 前端基础
+
+### 1. html css js
+
+- html骨骼、css皮肤、js肌肉
---
+++ spider/socket_test/http_server.py
@@ -0,0 +1,35 @@
+# 服务端
+import socket
+import threading
+
+server = socket.socket()
+server.bind(('0.0.0.0',8000))
+server.listen()
+
+def handle_sock(sock,addr):
+    while True:
+        # recv是阻塞的
+        tmp_data = sock.recv(1024)
+        print(tmp_data.decode('utf-8'))
+        response_template = '''HTTP/1.1 200 OK
+     
+<html>
+    <head>
+        <title>Web Server</title>
+    </head>
+    <body>
+        Hello world!
+    </body>
+</html>
+
+'''
+
+        # input_data = input()
+        sock.send(response_template.encode("utf8"))
+
+while True:
+    sock,addr = server.accept()
+    client_thread = threading.Thread(target=handle_sock, args=(sock,addr))
+    client_thread.start()
+
+
